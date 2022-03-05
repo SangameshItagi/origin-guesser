85c274f60812eef2591d44d588dc09e46b6c19d2
Sangamesh Itagi <itagi.sangamesh@gmail.com>
2022-03-05 14:20:44 -0500

    Added the score counter, fetching question and answer texts from json

--- src/App.js
+++ src/App.js
@@ -1,57 +1,107 @@
-import './App.css';
+
 import { Button } from 'antd';
-//import 'antd/dist/antd.css'
+
+import React, { useState } from 'react';
+
+export default function App() {
+  const questions = [
+      {
+          questionText: 'Piano',
+          answerOptions: [
+              { answerText: 'USA', isCorrect: false },
+              { answerText: 'UK', isCorrect: false },
+              { answerText: 'Germany', isCorrect: false },
+              { answerText: 'Japan', isCorrect: true },
+          ],
+      },
+      {
+          questionText: 'Compass',
+          answerOptions: [
+              { answerText: 'USA', isCorrect: false },
+              { answerText: 'UK', isCorrect: false },
+              { answerText: 'Germany', isCorrect: false },
+              { answerText: 'Japan', isCorrect: true },
+          ],
+      },
+      {
+          questionText: 'Printing Press',
+          answerOptions: [
+              { answerText: 'USA', isCorrect: false },
+              { answerText: 'UK', isCorrect: false },
+              { answerText: 'Germany', isCorrect: false },
+              { answerText: 'Japan', isCorrect: true },
+          ],
+      },
+      {
+          questionText: 'Cheese',
+          answerOptions: [
+              { answerText: 'USA', isCorrect: false },
+              { answerText: 'UK', isCorrect: false },
+              { answerText: 'Germany', isCorrect: false },
+              { answerText: 'Japan', isCorrect: true },
+          ],
+      },
+  ];
+
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [showScore, setShowScore] = useState(false);
+	const [score, setScore] = useState(0);
+
+	const handleAnswerOptionClick = (isCorrect) => {
+		if (isCorrect) {
+			setScore(score + 1);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
+		}
+	};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to OriginGuesser</h1>
-        <h2>Points:</h2>
-        <Button
-        className='option-One-Button' 
-        style = {{ flexWrap: "wrap"}}
-        style={{
-          
-          position: 'absolute',
-          left: '10%',
-          //alignConten
-          alignSelf: 'center-100'-100,
-          bottom: 100,
-          width: '20%', height: 200
-        }}
-        >Option 1</Button>
-        <Button
-        className='option-Two-Button' 
-        style={{
-          position: 'absolute',
-          left: '30%',
-          bottom: 100,
-          width: '20%', height: 200
-        }}
-        >Option 2</Button>
-        <Button
-        className='option-Three-Button' 
-        style={{
-          position: 'absolute',
-          right: '30%',
-          bottom: 100,
-          width: '20%', height: 200
-        }}
-        >Option 3</Button>
-        <Button
-        className='option-Four-Button' 
-        style={{
-          position: 'absolute',
-          right: '10%',
-          bottom: 100,
-          width: '20%', height: 200
-        }}
-        >Option 4</Button>
-        
+      <h1>Welcome to OriginGuesser</h1>
+      <h2>Points:</h2>
+        <Button className='option-One-Button'>Option 1</Button>
+        <Button className='option-Two-Button'>Option 2</Button>
+        <Button className='option-Three-Button'>Option 3</Button>
+        <Button className='option-Four-Button'>Option 4</Button>
       </header>
     </div>
   );
 }
 
-export default App;
+	return (
+		<div className='app'>
+      <header className="App-header">
+      <h1>Welcome to OriginGuesser</h1>
+      
+			{showScore ? (
+				<div className='score-section'>
+					You scored {score}
+				</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Score {score}</span>
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+						{questions[currentQuestion].answerOptions.map((answerOption) => (
+							<Button className='option-One-Button' onClick={() => handleAnswerOptionClick(answerOption.isCorrect)}>{answerOption.answerText}</Button>
+              
+            ))}
+					</div>
+				</>
+			)}
+      </header>
+		</div>
+    
+	);
+}
